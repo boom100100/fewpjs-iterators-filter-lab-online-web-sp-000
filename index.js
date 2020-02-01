332a7f934b5c2684ff8e8b347d0703db36130ba8
Bernadette Davis <bsd231@nyu.edu>
2020-02-01 01:07:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function fuzzyMatch(drivers, searchValue){
 }
 
 function matchName(drivers, searchValue){
-  let match = drivers.filter(driver => {
+  return drivers.filter(driver => {
     driver.name === searchValue;
   });
 }
